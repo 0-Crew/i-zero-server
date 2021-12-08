71f13d155dc7c8b4afa85e54ed73577fdbc8926d
suzieep <wnsuwls0707@gmail.com>
2021-12-08 16:45:11 +0900

    feat: challengeAdd

--- functions/api/routes/challenge/index.js
+++ functions/api/routes/challenge/index.js
@@ -3,5 +3,6 @@ const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 router.post('/add', checkUser, require('./challengeAddPOST'));
+router.get('/add', checkUser, require('./challengeAddGET'));
 
 module.exports = router;
