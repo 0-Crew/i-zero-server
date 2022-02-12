873d5bd9f14531c6f0a5cb69aa5f1b1c41cc95b8
realwhyjay <brandon94_@naver.com>
2022-02-12 18:09:42 +0900

    [chore] 불필요한 import 삭제

--- functions/api/routes/bottleworld/bottleworldFollowUnfallowPOST.js
+++ functions/api/routes/bottleworld/bottleworldFollowUnfallowPOST.js
@@ -1,11 +1,8 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 const util = require('../../../lib/util');
 const statusCode = require('../../../constants/statusCode');
 const responseMessage = require('../../../constants/responseMessage');
 const db = require('../../../db/db');
 const { myFollowingDB } = require('../../../db');
-const jwtHandlers = require('../../../lib/jwtHandlers');
 
 /*
 1. 클라이언트로부터 토큰을 받아 유저의 id를 받고, body를 통해 해당 유저가 팔로우 할 유저의 id를 받아온다.
