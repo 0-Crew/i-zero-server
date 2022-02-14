d881a18657f515f03394e83ef46a321d4e736775
realwhyjay <brandon94_@naver.com>
2022-02-14 15:57:48 +0900

    [add] following 하는 유저 조회 81퍼 완성

--- functions/api/routes/bottleworld/bottleworldFollowingGET.js
+++ functions/api/routes/bottleworld/bottleworldFollowingGET.js
@@ -14,31 +14,31 @@ module.exports = async (req, res) => {
   try {
     client = await db.connect(req);
     const followingUsers = await myFollowingDB.getFollowingUsers(client, user.id, keyword);
-    console.log('followings : ', followingUsers);
+    // console.log('followings : ', followingUsers);
 
     const userIds = arrayHandlers.extractValues(followingUsers, 'id');
-    console.log('userIds : ', userIds);
+    // console.log('userIds : ', userIds);
 
     const userChallenges = await myFollowingDB.getUsersChallenge(client, userIds);
-    console.log('userChallenges : ', userChallenges);
+    // console.log('userChallenges : ', userChallenges);
 
     const challengesForUsers = followingUsers.reduce((acc, x) => {
       acc[x.id] = { user: { ...x }, challenge: {} };
       return acc;
     }, {});
-    console.log('challengesForUsers :', challengesForUsers);
+    // console.log('challengesForUsers :', challengesForUsers);
 
     // ^_^// answerId로 그룹화 해준 answers들에 keywords를 넣어준다..
     userChallenges.map((o) => {
       challengesForUsers[o.id].challenge = o;
       return o;
     });
-    console.log('challengesForUsers22 : ', challengesForUsers);
+    // console.log('challengesForUsers22 : ', challengesForUsers);
 
     const result = Object.entries(challengesForUsers).map(([id, data]) => ({ ...data }));
     console.log('result : ', result);
 
-    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.GET_FOLLOWINGS_SUCCESS));
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.GET_FOLLOWINGS_SUCCESS, result));
   } catch (error) {
     // 서버 에러시 500 return
     res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
