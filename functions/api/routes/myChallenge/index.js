bffc9f9177aa10b0fa6daa91d0d2eebd1c801ba2
Suzie Lee <wnsuwls0707@gmail.com>
2022-05-29 23:46:16 +0200

    feat: 다른 유저 챌린지 캘린더 보기 및 현재 챌린지나 선택한 챌린지 없을 때 비우기

--- functions/api/routes/myChallenge/index.js
+++ functions/api/routes/myChallenge/index.js
@@ -6,6 +6,7 @@ router.post('/add', checkUser, require('./myChallengeAddPOST'));
 router.get('/add', checkUser, require('./myChallengeAddGET'));
 router.get('/main', checkUser, require('./myChallengeMainGET'));
 router.get('/user', checkUser, require('./myChallengeUserGET'));
+router.get('/user/calendar', checkUser, require('./myChallengeUserCalendarGET'));
 
 router.get('/calendar', checkUser, require('./myChallengeCalendarGET'));
 
